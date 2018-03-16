78b7b1b85f35addb61dc6cb8c232e70c9be71018
Joon Hee Lee <leejunhee2@gmail.com>
2018-03-15 19:07:10 -0700

    Initial visualization webapp

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 from flask import Flask, render_template
+from flask import url_for
+from os import listdir
+
+
+courses = [file.rstrip(".tsv") for file in filter(lambda x: ".tsv" in x, listdir("static/data"))]
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return render_template("index.html")
+@app.route("/index")
+def index():
+    return render_template("index.html", courses=courses)
 
 
+@app.route('/viz/<course>')
+def viz(course):
+    course_name = " ".join(course.split("_"))
+    return render_template("viz.html", data_path=url_for('static', filename='data/%s.tsv' % course), course_name = course_name, courses=courses)
 
 if __name__ == "__main__":
+    app.debug = True
     app.run()
